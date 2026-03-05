aa3d52151ab981f1aa347e0afed4700aac47a843
danclangcoder <nielbandahala@gmail.com>
2026-03-06 01:16:42 +0800

    login dashboard containers

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class AppWindow(customtkinter.CTk):
         super().__init__()
         self.title('Spreadsheets Management System')
         self.geometry('1920x1080')
-        self.minsize(800, 800)
+        self.minsize(800, 500)
         self.maximize_window()
 
         self.container = Frame(self)
@@ -36,35 +36,45 @@ class AppWindow(customtkinter.CTk):
         self.build_login_widgets()
 
         # Dashboard / Home / App
-        self.dashboard_frame = SFrame(self.container, fg_color=('#b1b1b1', '#202020'))
+        self.dashboard_frame = Frame(self.container, fg_color=('#b1b1b1', '#202020'))
         self.sidebar = Frame(self.dashboard_frame, width=50, fg_color=("#dbdbdb", "#2A2A2A"))
         self.sidebar.pack(side='left', fill='y')
-        self.body = Frame(self.dashboard_frame)
+        
+        self.top_layer = Frame(self.dashboard_frame)
+        self.top_layer.pack(fill='x', padx=10, pady=(10, 5))
+
+        self.body = SFrame(self.dashboard_frame)
         self.body.pack(expand=True, fill='both')
-        self.welcome_title = Label(self.body, text="Dashboard", font=('Segoe UI Black', 36, 'bold'))
-        self.welcome_title.pack(pady=50)
-        self.top_layer = Frame(self.body, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=40)
-        self.top_layer.pack(fill='x', padx=20, pady=20)
+        self.container_1 = Frame(self.body, height=200, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=35)
+        self.container_2 = Frame(self.body, height=200, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=35)
+        self.container_3 = Frame(self.body, height=200, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=35)
+        self.container_1.pack(fill='x', padx=20, pady=20)
+        self.container_2.pack(fill='x', padx=20, pady=20)
+        self.container_3.pack(fill='x', padx=20, pady=20)
         self.build_dashboard_widgets()
 
     def build_login_widgets(self):
         # Login Widgets
-        self.login_title = Label(self.login_frame, text='Login or Register', font=('Segoe UI Black', 24, 'bold'))
+        self.login_title = Label(self.login_frame, text='Login', font=('Segoe UI Black', 24, 'bold'))
         self.login_title.place(relx=0.5, rely=0.15, anchor='center')
         self.username = Entry(self.login_frame, width=250, height=30, placeholder_text='Username')
         self.username.place(relx=0.5, rely=0.30, anchor='center')
         self.password = Entry(self.login_frame, width=250, height=30, placeholder_text='Password', show='*')
         self.password.place(relx=0.5, rely=0.40, anchor='center')
-        self.register_button = Button(self.login_frame, text='Register', command=self.register_user)
-        self.register_button.place(relx=0.65, rely=0.85, anchor='center')
         self.login_button = Button(self.login_frame, text='Login', command=self.login_user)
-        self.login_button.place(relx=0.35, rely=0.85, anchor='center')
+        self.login_button.place(relx=0.5, rely=0.85, anchor='center')
+
+        # self.register_button = Button(self.login_frame, text='Register', command=self.register_user)
+        # self.register_button.place(relx=0.65, rely=0.85, anchor='center')
 
     def build_dashboard_widgets(self):
-        now = datetime.now()
-        formatted_date = now.strftime("%m/%d/%Y")
+        date_today = datetime.now()
+        formatted_date = date_today.strftime("%B %d, %Y")
         self.date_today = Label(self.top_layer, text=formatted_date)
-        self.date_today.place(relx=0.8, rely=0.2, anchor='center')
+        self.date_today.place(relx=0.95, rely=0.2, anchor='center')
+
+        self.welcome_title = Label(self.top_layer, text="Dashboard", font=('Segoe UI Black', 36, 'bold'))
+        self.welcome_title.pack(pady=10)
 
     def register_user(self):
         # username, password = [self.username.get(), self.password.get()]
--- frames.py
+++ frames.py
@@ -2,9 +2,9 @@ import customtkinter
 
 
 class Frame(customtkinter.CTkFrame):
-    def __init__(self, parent, width=None, height=None, fg_color='transparent', corner_radius=0):
+    def __init__(self, parent, width=None, height=None, bg_color='transparent', fg_color='transparent', corner_radius=0, border_color='', border_width=0):
         super().__init__(parent)
-        self.configure(width=width, height=height, fg_color=fg_color, corner_radius=corner_radius)
+        self.configure(width=width, height=height, bg_color=bg_color, fg_color=fg_color, corner_radius=corner_radius, border_color=border_color, border_width=border_width)
 
 class SFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, parent, width=None, height=None, fg_color='transparent', corner_radius=0):
