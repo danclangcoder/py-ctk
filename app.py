3ce911430e4ce1c401bca72d6d43992f53e1bce2
unknown <nielbandahala@gmail.com>
2026-03-05 18:25:40 +0800

    Login and Dashboard

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import customtkinter
+from tkinter import messagebox
 from frames import Frame, SFrame
-from widgets import Button, Entry
-from hashlib import sha
+from widgets import Button, Entry, Label
+from database import init_db
+from auth import register, login
+from datetime import datetime
 
+
+init_db()
 
 # customtkinter.set_appearance_mode('light')
 customtkinter.set_appearance_mode('dark')
@@ -12,9 +17,12 @@ class AppWindow(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title('Spreadsheets Management System')
-        # self.geometry('1920x1080')
-        self.minsize(800, 600)
-        # self.maximize_window()
+        self.geometry('1920x1080')
+        self.minsize(800, 800)
+        self.maximize_window()
+
+        self.container = Frame(self)
+        self.container.pack(expand=True, fill='both')
 
         self.build_frames()
         
@@ -22,32 +30,67 @@ class AppWindow(customtkinter.CTk):
         self.after(10, lambda: self.state('zoomed'))
 
     def build_frames(self):
-        self.sidebar = Frame(self, fg_color=('#b1b1b1', '#202020'))
+        # Login Screen
+        self.login_frame = Frame(self.container, width=400, height=450, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=30)
+        self.login_frame.pack(pady=(200, 0))
+        self.build_login_widgets()
 
-        self.body = SFrame(self)
+        # Dashboard / Home / App
+        self.dashboard_frame = SFrame(self.container, fg_color=('#b1b1b1', '#202020'))
+        self.sidebar = Frame(self.dashboard_frame, width=50, fg_color=("#dbdbdb", "#2A2A2A"))
+        self.sidebar.pack(side='left', fill='y')
+        self.body = Frame(self.dashboard_frame)
         self.body.pack(expand=True, fill='both')
+        self.welcome_title = Label(self.body, text="Dashboard", font=('Segoe UI Black', 36, 'bold'))
+        self.welcome_title.pack(pady=50)
+        self.top_layer = Frame(self.body, fg_color=("#dbdbdb", "#2A2A2A"), corner_radius=40)
+        self.top_layer.pack(fill='x', padx=20, pady=20)
+        self.build_dashboard_widgets()
 
-        self.login_frame = Frame(self.body, width=400, height=600, fg_color=('#b1b1b1', "#2A2A2A"), corner_radius=30)
-        self.login_frame.pack(pady=(100, 0))
-        self.build_widgets(self.login_frame)
-
-    def build_widgets(self, frame):
-        self.username = Entry(frame, width=250, height=30, placeholder_text='Username')
-        self.username.place(relx=0.5, rely=0.36, anchor='center')
-        self.password = Entry(frame, width=250, height=30, placeholder_text='Password')
-        self.password.place(relx=0.5, rely=0.42, anchor='center')
-        self.register_button = Button(parent=frame, text='Register', command=self.register_user)
+    def build_login_widgets(self):
+        # Login Widgets
+        self.login_title = Label(self.login_frame, text='Login or Register', font=('Segoe UI Black', 24, 'bold'))
+        self.login_title.place(relx=0.5, rely=0.15, anchor='center')
+        self.username = Entry(self.login_frame, width=250, height=30, placeholder_text='Username')
+        self.username.place(relx=0.5, rely=0.30, anchor='center')
+        self.password = Entry(self.login_frame, width=250, height=30, placeholder_text='Password', show='*')
+        self.password.place(relx=0.5, rely=0.40, anchor='center')
+        self.register_button = Button(self.login_frame, text='Register', command=self.register_user)
         self.register_button.place(relx=0.65, rely=0.85, anchor='center')
-        self.login_button = Button(parent=frame, text='Login', command=self.login)
+        self.login_button = Button(self.login_frame, text='Login', command=self.login_user)
         self.login_button.place(relx=0.35, rely=0.85, anchor='center')
 
-    def register_user(self):
-        username, password = [self.username.get(), self.password.get()]
-        self.register_button.register_user(username, password)
-    
-    def login(self):
-        self.login_button.login()
+    def build_dashboard_widgets(self):
+        now = datetime.now()
+        formatted_date = now.strftime("%m/%d/%Y")
+        self.date_today = Label(self.top_layer, text=formatted_date)
+        self.date_today.place(relx=0.8, rely=0.2, anchor='center')
 
+    def register_user(self):
+        # username, password = [self.username.get(), self.password.get()]
+        username = self.username.get()
+        password = self.password.get()
+        success, message = register(username, password)
+        if not success:
+            messagebox.showerror('Register error', message)
+            print('ERROR', message)
+        else:
+            messagebox.showinfo('Success', message)
+            print(message)
+    
+    def login_user(self):
+        username, password = [self.username.get(), self.password.get()]
+        success, message = login(username, password)
+        if not success:
+            messagebox.showerror('Login attempt failed', message)
+            print('ERROR', message)
+        else:
+            self.show_dashboard()
+            print(message)
+
+    def show_dashboard(self):
+        self.dashboard_frame.pack(expand=True, fill='both')
+        self.login_frame.pack_forget()
 
 if __name__ == '__main__':
     app = AppWindow()
--- widgets.py
+++ widgets.py
@@ -2,18 +2,43 @@ import customtkinter
 
 
 class Button(customtkinter.CTkButton):
-    def __init__(self, parent, width=None, height=None, fg_color='transparent', corner_radius=0, text=None, command=None):
-        super().__init__(parent, width=100, height=40, fg_color=("#4483E1", "#3F5A8E"), corner_radius=10)
+    def __init__(
+        self,
+        parent,
+        width=None,
+        height=None,
+        text=None,
+        command=None,
+    ):
+        super().__init__(
+            parent,
+            width=100,
+            height=40,
+            fg_color=("#185CC2", "#3F5A8E"),
+            hover_color=("#1D3D6E", "#22427E"),
+            corner_radius=10,
+        )
         self.configure(width=width, height=height, text=text, command=command)
 
-    def register_user(self, username, password):
-        print(f'{username}\n{password}')
-    
-    def login(self):
-        print('User logged in.')
-    
 
 class Entry(customtkinter.CTkEntry):
-    def __init__(self, parent, width, height, placeholder_text):
+    def __init__(self, parent, width, height, placeholder_text, show=None):
         super().__init__(parent, corner_radius=12)
-        self.configure(width=width, height=height, placeholder_text=placeholder_text)
+        self.configure(
+            width=width, height=height, placeholder_text=placeholder_text, show=show
+        )
+
+
+class Label(customtkinter.CTkLabel):
+    def __init__(
+        self,
+        parent,
+        text=None,
+        font=(None, 0),
+        fg_color="transparent",
+        corner_radius=0,
+    ):
+        super().__init__(parent)
+        self.configure(
+            text=text, font=font, fg_color=fg_color, corner_radius=corner_radius
+        )
